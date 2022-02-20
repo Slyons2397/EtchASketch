67653f5902b2056aa7639a5720fe9f918a244b37
Samuel Lyons <samuellyons@Samuels-MacBook-Air.local>
2022-02-20 16:36:06 -0500

    Finished project

---
+++ game.js
@@ -0,0 +1,57 @@
+//credit to Andrew Koenig-Bautista at 'levelup' for the HTML, JS, and CSS template/start.//
+
+
+const grid = document.querySelector(".gridContainer");
+const userInput = document.getElementById("quantity");
+const resetButton = document.querySelector(".reset");
+
+createGrid = () => {
+  for (let i = 0; i < 256; i++) {
+    const div = document.createElement("div");
+    div.classList.add("square");
+    grid.appendChild(div);
+  }
+};
+
+updateGrid = () => {
+  grid.innerHTML = "";
+  grid.style.setProperty(
+    "grid-template-columns",
+    `repeat(${userInput.value}, 2fr)`
+  );
+  grid.style.setProperty(
+    "grid-template-rows",
+    `repeat(${userInput.value}, 2fr)`
+  );
+  for (let i = 0; i < userInput.value * userInput.value; i++) {
+    const div = document.createElement("div");
+    div.classList.add("square");
+    grid.appendChild(div);
+  }
+  console.log(userInput.value);
+};
+
+const square = document.querySelector("div");
+square.addEventListener("mouseover", function(event) {
+  event.target.classList.replace("square", "color");
+  square.addEventListener("click", function(event){
+      event.target.classList.replace("square", "color2")
+  })
+});
+
+
+userInput.addEventListener("change", updateGrid);
+
+resetButton.addEventListener("click", function() {
+  grid.innerHTML = "";
+  userInput.value = "";
+  grid.style.setProperty("grid-template-columns", `repeat(16, 2fr)`);
+  grid.style.setProperty("grid-template-rows", `repeat(16, 2fr)`);
+  alert ("It is gone!");
+  createGrid();
+});
+
+
+
+
+createGrid();
